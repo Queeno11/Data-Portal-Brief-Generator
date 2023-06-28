44cad609ebdd96026f7d2fe045d7587e32286f53
yanelllohis <llohisyanel@gmail.com>
2023-06-28 12:53:07 -0300

    R scripts: tex and adjusted margins

--- 11_Run_Briefs.R
+++
@@ -1,8 +0,0 @@
-
-# Loop over each country
-for (country in countrynamet) {
-  output_file <- paste0("brief_", country, ".pdf")
-  rmarkdown::render(input = "AM_2023_HC.Rmd", output_format = "pdf_document",
-                    output_file = output_file, params = list(countrynamet = country))
-}
-
