3a7d7c081cd659fca642438cfa958ee4e40e47db
Nicolás Abbate <57244513+Queeno11@users.noreply.github.com>
2025-07-04 18:31:44 -0300

    Add both 1 and 2 pages versions

--- 10_add_header_and_footer.py
+++ 10_add_header_and_footer.py
@@ -1,3 +1,5 @@
+import argparse
+
 try:
     from sfi import Macro
 
@@ -28,7 +30,20 @@ import pandas as pd
 from tqdm import tqdm
 import fitz 
 
-footer_path = os.path.join(sources, "Footer Images", "Footer - QR HC Data Portal.png")
+# Parse args to ensure if it 1 page or two pages
+parser = argparse.ArgumentParser(description="Add header and footer to PDF")
+parser.add_argument("--two-pages", action="store_true", help="Indicate if the PDF has two pages")
+args = parser.parse_args()
+two_pages = args.two_pages
+
+if two_pages:
+    footer_path = [
+        os.path.join(sources, "Footer Images", "p1 footer.png"),
+        os.path.join(sources, "Footer Images", "p2 footer.png")
+    ]
+    
+else:
+    footer_path = [os.path.join(sources, "Footer Images", "Footer - QR HC Data Portal.png")]
 
 def fix_dataportal_country_names(name):
     ''' IT guys changed some of the briefs original names. This function transforms the original names to the new ones. '''
@@ -95,44 +110,50 @@ def list_files_in_directory(folder_path):
             file_list.append(os.path.join(root, file))
     return file_list
 
-def add_header_and_footer(doc_path, header_path, out_path):
+def add_header_and_footer(doc_path, header_path, out_path, two_pages=False):
 
     # Open the existing PDF
     doc = fitz.open(doc_path)
     
-    # Get the dimensions of the image
-    header = fitz.open(header_path)
-    header_width, header_height = header[0].rect.width, header[0].rect.height
-    header.close()
+    if two_pages:
+        page_numbers = range(0, 2)
+    else:
+        page_numbers = [0]
+    
+    for page_number in page_numbers:
 
-    footer = fitz.open(footer_path)
-    footer_width, footer_height = footer[0].rect.width, footer[0].rect.height
-    footer.close()
+        # Get the dimensions of the image
+        header = fitz.open(header_path[page_number])
+        header_width, header_height = header[0].rect.width, header[0].rect.height
+        header.close()
 
+        footer = fitz.open(footer_path[page_number])
+        footer_width, footer_height = footer[0].rect.width, footer[0].rect.height
+        footer.close()
+        
+        page = doc[page_number]
+        # Get the page dimensions
+        page_width = page.rect.width
+        page_height = page.rect.height
 
-    page = doc[0]
-    # Get the page dimensions
-    page_width = page.rect.width
-    page_height = page.rect.height
+        # Calculate the rectangle where the image will be placed
+        # This example scales the image to fit the width of the page
+        header_rect = fitz.Rect(
+            0,  # x0: left side of the page
+            0,  # y0: top of the page
+            page_width,  # x1: right side of the page
+            header_height * (page_width / header_width)  # y1: maintain aspect ratio
+        )
+        footer_rect = fitz.Rect(
+            0,  # x0: left side of the page
+            page_height - footer_height * (page_width / footer_width),  # y0: bottom
+            page_width,  # x1: right side of the page
+            page_height  # y1: maintain aspect ratio
+        )
 
-    # Calculate the rectangle where the image will be placed
-    # This example scales the image to fit the width of the page
-    header_rect = fitz.Rect(
-        0,  # x0: left side of the page
-        0,  # y0: top of the page
-        page_width,  # x1: right side of the page
-        header_height * (page_width / header_width)  # y1: maintain aspect ratio
-    )
-    footer_rect = fitz.Rect(
-        0,  # x0: left side of the page
-        page_height - footer_height * (page_width / footer_width),  # y0: bottom
-        page_width,  # x1: right side of the page
-        page_height  # y1: maintain aspect ratio
-    )
-
-    # Insert the image into the page
-    page.insert_image(header_rect, filename=header_path)
-    page.insert_image(footer_rect, filename=footer_path)
+        # Insert the image into the page
+        page.insert_image(header_rect, filename=header_path[page_number])
+        page.insert_image(footer_rect, filename=footer_path[page_number])
 
     # Save the modified PDF to a new file
     doc.ez_save(out_path)
@@ -145,23 +166,26 @@ df = df[df.wbcode.isin(["KEN"])]
 os.makedirs(rf"{briefs}\For Print", exist_ok=True)
 headers = list_files_in_directory(rf"{sources}\\Header Images\\Headers pngs")
 for country_data in tqdm(df[["wbcode", "wbcountryname", "wbregion"]].itertuples(), total=df.shape[0]):
-    # try:
-    wbcode = country_data[1]
-    wbcountryname = country_data[2]
-    wbregion = country_data[3]
-
-    if wbcode == "BIH":
-        wbcode = "BIS"
-
     try:
+        wbcode = country_data[1]
+        wbcountryname = country_data[2]
+        wbregion = country_data[3]
+
+        if wbcode == "BIH":
+            wbcode = "BIS"
+
+        # try:
         brief_path = rf"{briefs}\{wbcountryname}\{wbcountryname}{extra}.pdf"
-        header_path = [header for header in headers if f"1-HCCB-{wbcode}" in header][0]
+        header_path = [header for header in headers if f"HCCB-{wbcode}" in header]
+        if two_pages is False:
+            header_path = [header_path[0]]
+            
         out_folder = rf"{briefs}\For print\{wbregion}"
         os.makedirs(out_folder, exist_ok=True)
         outname = fix_dataportal_country_names(wbcountryname)
         out_path = rf"{out_folder}\{outname}{extra}.pdf"
 
-        add_header_and_footer(brief_path, header_path, out_path)
+        add_header_and_footer(brief_path, header_path, out_path, two_pages=two_pages)
 
     except Exception as exception:
         print(f"Error with {wbcode}: {exception}")
